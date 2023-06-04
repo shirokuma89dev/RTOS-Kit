603e3c2710075f35b8dd62965faac03cd4d8af15
shirokuma89dev <code@shirokuma89.dev>
2023-06-04 16:02:40 +0900

    1.1.0

--- src/RTOS-Kit.cpp
+++ src/RTOS-Kit.cpp
@@ -7,8 +7,9 @@ void RTOS_Kit::create(TaskFunction_t funcPtr, int priority) {
     for (int i = 0; i < maximumAppCount; i++) {
         if (funcPtrArray[i] == NULL) {
             funcPtrArray[i] = funcPtr;
+            priorityArray[i] = priority;
 
-            xTaskCreate(funcPtrArray[i], "app", 1000, NULL, priority,
+            xTaskCreate(funcPtrArray[i], "app", 256, NULL, priority,
                         &handlerArray[i]);
             vTaskSuspend(handlerArray[i]);
 
@@ -20,6 +21,19 @@ void RTOS_Kit::create(TaskFunction_t funcPtr, int priority) {
 void RTOS_Kit::start(TaskFunction_t funcPtr) {
     for (int i = 0; i < maximumAppCount; i++) {
         if (funcPtrArray[i] == funcPtr) {
+            vTaskResume(handlerArray[i]);
+
+            break;
+        }
+    }
+}
+
+void RTOS_Kit::restart(TaskFunction_t funcPtr) {
+    for (int i = 0; i < maximumAppCount; i++) {
+        if (funcPtrArray[i] == funcPtr) {
+            vTaskDelete(handlerArray[i]);
+            xTaskCreate(funcPtrArray[i], "app", 256, NULL, priorityArray[i],
+                        &handlerArray[i]);
             vTaskResume(handlerArray[i]);
 
             break;
--- src/RTOS-Kit.h
+++ src/RTOS-Kit.h
@@ -4,7 +4,7 @@
 #include <STM32FreeRTOS.h>
 
 #define App void *rtosKit
-#define maximumAppCount 10
+#define maximumAppCount 30
 
 #define firstPriority 4
 #define secondPriority 3
@@ -17,16 +17,16 @@ class RTOS_Kit {
     void create(TaskFunction_t funcPtr, int priority = defaultPriority);
     void start(TaskFunction_t funcPtr);
     void stop(TaskFunction_t funcPtr);
+    void restart(TaskFunction_t funcPtr);
 
     void startRTOS(void);
-
     void delay(unsigned long ms);
 
     int mainAppNumber  = 0;
+    int priorityArray[maximumAppCount];
 
     TaskFunction_t funcPtrArray[maximumAppCount];
     TaskHandle_t handlerArray[maximumAppCount];
-
    private:
 };
 
